09bbfe73adc94ac738a818284942f822f4df9efb
zi.peng <44833128+961163289@users.noreply.github.com>
2022-08-19 00:04:13 +0800

    项目瘦身,实现后端接口请求

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,29 +1,33 @@
+import { PLANET_LINK } from '@/constants';
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = '鱼皮出品';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
+        /*修改页脚信息*/
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          /*key随便起,只要保证在该links中唯一即可*/
+          key: 'planet',
+          title: '知识星球',
+          href: PLANET_LINK,
+          /*blankTarget: 设置是当前页面打开,还是在新的页面打开*/
           blankTarget: true,
         },
         {
           key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          title: '编程导航',
+          href: 'https://www.code-nav.cn',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <><GithubOutlined />鱼皮 GitHub</>,
+          href: 'https://github.com/961163289',
           blankTarget: true,
         },
       ]}
